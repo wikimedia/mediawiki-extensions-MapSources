228694ba3d0e689a7b95cfb6c1d7f92a26cfef84
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 20:50:59 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ia7bf157e6206a28ce2caa162c55ceeb0b205b080

--- MapSources.php
+++ MapSources.php
@@ -17,12 +17,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['MapSources'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['MapSourcesAlias'] = __DIR__ . '/MapSources.i18n.alias.php';
 	$wgExtensionMessagesFiles['MapSourcesMagic'] = __DIR__ . '/MapSources.i18n.magic.php';
-	/*wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for MapSources extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);*/
+	);
 	return;
 } else {
-	die( 'This version of the MapSources extension requires MediaWiki 1.25+' );
+	die( 'This version of the MapSources extension requires MediaWiki 1.32+' );
 }
