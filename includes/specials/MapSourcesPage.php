1c2e8eb6deb9359621a5844a626a0ec61008a84d
James D. Forrester <jforrester@wikimedia.org>
2019-05-19 13:22:50 +0200

    build: Upgrade mediawiki/mediawiki-phan-config from 0.5.0 to 0.6.0
    Change-Id: I74b8cb49ce41f5cf187b2450c86c4f00a8c9bd24

--- includes/specials/MapSourcesPage.php
+++ includes/specials/MapSourcesPage.php
@@ -189,6 +189,7 @@ class MapSourcesPage extends SpecialPage {
 			}
 		}
 		if ( isset( $this->par['precision'] ) ) {
+			/** @phan-suppress-next-line PhanTypeInvalidDimOffset */
 			$a = intval( $this->par['precision'] );
 			if ( ( $a < 0 ) || ( $a > 12 ) ) {
 				$a = 6;
